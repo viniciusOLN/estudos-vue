8bb46f3a49ab347cb53645963ef1d63c91b42051
Vinicius Oliveira <vinicius853o@gmail.com>
2020-10-14 18:55:23 -0300

    Estudos - Criação de um componente em vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-  
+    <cliente/>
   </div>
 </template>
 
 <script>
 
+import cliente from './components/cliente'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    cliente
+  }
 }
 </script>
 
 <style>
+#app{
+  
+}
 
 </style>
--- src/components/cliente.vue
+++ src/components/cliente.vue
@@ -1,5 +1,8 @@
 <template>
-    
+    <div class="batata">
+    <h2>Cliente aqui!</h2>
+    <h3>desc cliente</h3>    
+    </div>
 </template>
 
 <script>
@@ -9,5 +12,8 @@ export default {
 </script>
 
 <style scoped>
-
+    .batata{
+        color: rgb(192, 185, 185);
+        background: yellow;
+    }
 </style>
